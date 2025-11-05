bf3dc309813a18983c40aa61fb269f32bf52e59a
gtsaturyan <gtsaturyan@ozon.ru>
2025-11-05 21:46:45 +0300

    deploying pipeline

--- internal/clients/drive/client_test.go
+++ internal/clients/drive/client_test.go
@@ -2,14 +2,16 @@ package drive_test
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-
 	"testing"
 	"time"
 
 	"github.com/Armenian-Club/ak-onboarding/internal/clients/drive"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/oauth2"
 	"google.golang.org/api/option"
 )
 
@@ -61,26 +63,30 @@ func TestClient_AddUser(t *testing.T) {
 			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				tt.httpHandler(t, w, r)
 			}))
-			defer server.Close()
+			t.Cleanup(server.Close)
 
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
-
+			authServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				b, _ := json.Marshal(oauth2.Token{})
+				_, _ = w.Write(b)
+			}))
+			t.Cleanup(authServer.Close)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Hour)
+			t.Cleanup(cancel)
 			// Фиктивные креды для теста
-			fakeCreds := []byte(`{
+			fakeCreds := fmt.Appendf(nil, `{
 			  "type": "service_account",
 			  "project_id": "test-448519",
-			  "private_key_id": "c297bc0fee46f13e7fd7476065f3cd7b4a060b24",
+			  "private_key_id": "fakeid",
 			  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDW2rHK6+1eM2G9\nlPXTQHMEYlyvR1MKOp1kOsUL9EFiIFAJsTRT7dcKx3tnnIumGhN6L6rw5h6dhwLe\n9SLVdt4ioe089hdp00ayLFSzF3tT6Qq73oAsEoS00EZqgQPmmGL8+3nsMVwdro50\nMR1bb1ai5MdcT7bikh3h/aRgA8A2lnJSeHNt09i1hdUgAHrf8ZlhfZQ0DOqZeT8w\njRcsP4zQt4IO0wRMSdSIuc4yD/9wE1smCaqctEwrHv4PCHp8F0WprsmuR8UFUuNL\nFq/pfIJNj1/JBH63YtAQ5x+pJw2lmogD98w7rzWxQqbnQ5Z2PsRY0D+GSo6uoZj6\nJKqc7XUNAgMBAAECggEACRI8tXOWlwaWVtnGM0AiWwoIHcJmKCVnZcbxcNrEM+9n\nUbFwoyaEkMjxVeOPJdkt/1ep4PfmTQJZRa6V5Ota3510lcFSJb6s2nLytIkGRPmu\n4VW4laPGhJfSkUaXMpI2g7XeZPGEkSBAlXlJYwXlY4VDQYuADjrbFiKOYRtnbyaB\nKUzEgCAWNFIXn5SSKf81K+kBfOtIYhf6qWn1jxe381njzE8iw9Qlmq9mJWlyk1Cj\nPACJVecZ3j6hkViqxNvMJ8V3/VbIO0isBSO7xBCmdJ9lP9rQn9A/W6u9PH8ChKev\nsVquupoR2dEN7rYqpK1Zy6/TsDtqt/BVDPUB3ueWRQKBgQDrmpP7H319TmeG1WTv\ncYQRMLM6D7jNsTM/kIT9+ozH5GxGhccVun26bv57O1zN8cSJ8Pa+yy9n6eQvtoub\nZKYTslknCZdPoUQKceRMn4qvKgei3wCdpXG1qXplcea0/HjbOTB6KFuFiUoltBsl\nbSwnxJYGOX/Z8oPPx0lS+4jcewKBgQDpdESF6owsLjEXpJxO9sQ06aB/KAzwF+Me\nThhKr95Xd3UJjoLnipbFXmFpRzqkbIZHHB1v5Re9jMWCY8Dcut7r4Gyx1Y94EVre\nmh8nE+ADUODYdizpzR6bSl1gumuk1bTu2M6HhnBrbNBIOtPwWyM4p7D7xPbXlOI0\nZsEs5RMSFwKBgDr5vbMtxcbZGncY8aQyYSHAdAzDpLnwcmil73R4BEeBOU1J7XTV\n8uT5JcCJMojmzRDOfaVyzRIQ7Sq4YifqwNvLWB+6eeLX9mU67y/y+88vESxG8CuG\nH3mey+Ga6mpBjKsrnKPneElr/WCEvgrXUic+QWObfxJ6b15Sf1tDVZYDAoGBAK3p\nlZqFrkLDboMEeAVDQ8t/N7dCaND7mpBa8THCbkqOjTu/VLmUvtjthHffPkp7JlUX\nFr7i1Zq5ofGOyoAlHihuGcspIyX5F8641fhQkBMoTzgyYScTTXe2IHYMqmAzbAR6\nsvC3MEx21XrZiEWIP2bXVbtZceIL4a/T1JjTVi+lAoGBAIsHlLAooBVlIfcPV5S0\nh8rv8Do5chvJKqecbX+zmYao0DtvgikPOa4x474I1OoSaad/uXer1XvEiuY/Apnc\ncpJDvNUrwqh5Ixc//yo+4lM5MZJpO+Vgn6ifVEXJjVDMJxGM7WtCB15xYYDtt9n5\nBxZK0L51Prn18R+DKOcuRES9\n-----END PRIVATE KEY-----\n",
 			  "client_email": "testpr@test-448519.iam.gserviceaccount.com",
 			  "client_id": "104437331581182177415",
 			  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-			  "token_uri": "https://oauth2.googleapis.com/token",
+			  "token_uri": "%s",
 			  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-			  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testpr%40test-448519.iam.gserviceaccount.com",
+			  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testprest-448519.iam.gserviceaccount.com",
 			  "universe_domain": "googleapis.com"
-			}`)
-
+			}`, authServer.URL)
 			// Создаём клиент с фиктивными кредами
 			c, err := drive.NewClient(
 				ctx,
